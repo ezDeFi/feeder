ae29a8341c75ee287263371d1d8b3b29f467e258
Zergity <zergity@gmail.com>
2019-06-25 14:08:17 +0700

    add resetting data by chance

--- modules/price/random_nusd_usd.js
+++ modules/price/random_nusd_usd.js
@@ -3,17 +3,38 @@ const {promisify} = require('util');
 const requestAsync= promisify(request);
 
 var result = 1.0;
+var resetting = false;
 
 module.exports = {
 	fetchPrice: async function (pair) {
-		if (Math.random() < 0.3) {
-			var price = result + Math.random() * 0.2 - 0.1;
-			console.log(price + ' (outlier)');
-			return price;
-		} else {
-			result += Math.random() * 0.01 - 0.005;
-			console.log(result);
-			return result
+		if (!resetting && Math.random() < 0.03) {
+			// reset the price every once in a while
+			resetting = true;
+			console.log('start resetting');
 		}
+
+		let delta = Math.random() * 0.02 - 0.01;
+
+		if (resetting) {
+			delta = -Math.sign(result - 1.0) * Math.abs(delta)
+			resultNew = result + delta;
+			if (Math.sign(resultNew - 1.0) != Math.sign(result - 1.0)) {
+				resetting = false;
+				console.log('done resetting');
+			} else {
+				console.log('resetting');
+			}				
+		}
+
+		result += delta;
+		console.log(result);
+
+		if (Math.random() < 0.3) {
+			let outlier = result + delta * 3;
+			console.log(outlier, 'outlier');
+			return outlier;
+		}
+
+		return result;
 	}
 }
